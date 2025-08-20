1606062a1e5007b1b679b39cf94df1d439f60e56
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-19 22:34:49 -0500

    Delete ProjectLeviathan - Project directory

--- ProjectLeviathan - Project/ProjectLeviathan - Frontend/includes/sections/help/suggestions-section.php
+++
@@ -1,3 +0,0 @@
-<div class="section-content overflow-y <?php echo $CURRENT_SUBSECTION === 'suggestions' ? 'active' : 'disabled'; ?>" data-section="sectionSuggestions">
-    <h2>Enviar Sugerencias</h2>
-</div>
